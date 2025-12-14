4f8f24637091fa780760a9d7fd2cebe3a7bbaedf
Azizullah <azizullahcodes@gmail.com>
2025-12-14 13:17:56 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+/* small js problems and concepts */
+// 1 
+// console.log(String.raw`Hello\njavascript`)
+// String.raw js k aik built-in-method hy ic mai jaise string likhe hy vyse he show hoti hy 
+
+// 2. 
+// console.log(typeof NaN)
+// typeof NaN is number
+
+// 3. 
+// console.log(1 + '2' + '2'); ans = 122
+
+// 4. 
+/*
+console.log(3>2>1)  ans = false 
+why  
+js does not work like mathematics, first js see that  3 > 2 result true 
+now 
+true > 1 but 
+true value in js is = 1 
+so 
+1 > 1 
+ans is false
+*/
+
+// 5 
+/*
+ console.log(3>2 && 2 >1) 
+ ans = true ;
+ */
+
+//  6 
+/*
+console.log(1<3<4)  
+*/
+
+// 7 
+/*
+.sort()
+let number = [33,2,8];
+number = number.sort();
+console.log(number)
+*/
+
+// 8 
+/*
+let str = ['banan','mango','apple','guava'];
+str = str.sort();
+console.log(str)
+*/
+
+// 9 
+/*
+console.log(0.1 + 0.2 == 0.3)
+*/
+
+// 10 
+/*
+console.log(typeof typeof 1);
+ans = string 
+b/c 
+first checks typeof 1 that is number and and comes in string type
+and next typeof checks typeof number that answer is string 
+*/
+
+
+// 11 
+/*
+console.log(typeof typeof 'h')
+ans = string
+*/
